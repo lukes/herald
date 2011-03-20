6f4f859e5e19d367abb45107604ae8596c36bc48
Luke Duncalfe <l.duncalfe@auckland.ac.nz>
2011-03-21 11:56:44 +1300

    Implemented a test() for Notifier::Growl, and added error catching when system() calls to ruby-growl binary fail

--- lib/herald/notifiers/growl.rb
+++ lib/herald/notifiers/growl.rb
@@ -1,30 +1,43 @@
 class Herald
   class Watcher
     class Notifier
-      
+
       module Growl
-    
-        # TODO test growl on system and throw exception if fail
+
+#        # lazy-load ruby-growl when this Module is used as a Notifier
+#        def self.extended(base)
+#          begin
+#            Herald.lazy_load('ruby-growl')
+#          rescue LoadError
+#            raise LoadError, "ruby-growl is not installed"
+#          end
+#        end
+
+        # TODO test growl on system and throw exception if fail.
         def test
+          unless(system("growl --version"))
+            # TODO throw custom exception
+            raise "ruby-growl gem is not installed. Run:\n\tsudo gem install ruby-growl"
+          end
         end
-        
+
         # send a Growl notification
         def notify(title, message)
-          begin
-            `growl -H localhost -t #{title} -m #{message}`
-          rescue Errno::ECONNREFUSED => e
-            # TODO raise a custom Error
-            raise "Growl not installed, or not configured correctly to allow remote application\n
-                  registration. See http://growl.info/documentation/exploring-preferences.php"
+          # response will be false if command exits with an error
+          response = system("growl -H localhost -t #{title} -m #{message}")
+          # presence of ruby-growl has already been tested in test(), so a false
+          # return is likely to be the user's Growl settings
+          if !response
+            # TODO throw custom exception
+            raise "Growl settings not configured to allow remote application registration. See Growl website docs: http://growl.info/documentation/exploring-preferences.php"
           end
-          true
         end
-    
+
         # no options to parse for Growl
         def parse_options(options); end
-    
+
       end
-      
+
     end
   end  
 end
