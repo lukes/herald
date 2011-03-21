5a64d369e10fd105b9bb8adb7930f8ec0958caf0
Luke Duncalfe <l.duncalfe@auckland.ac.nz>
2011-03-21 15:34:45 +1300

    reordered methods

--- lib/herald/notifiers/ping.rb
+++ lib/herald/notifiers/ping.rb
@@ -11,20 +11,6 @@ class Herald
           Herald.lazy_load('net/http')
         end
 
-        # TODO test ping to URL on system and throw exception if fail
-        def test
-          Net::HTTP.new(@uri).head('/').kind_of?(Net::HTTPOK)
-        end
-
-        # TODO catch exception
-        def notify(title, message)
-          begin
-            # test response for 200 status
-            Net::HTTP.new(@uri).head('/').kind_of?(Net::HTTPOK)
-          rescue 
-          end
-        end
-
         def parse_options(options)
           begin
             @uri = URI.parse(options.delete(:uri) || options.delete(:url))
@@ -32,6 +18,15 @@ class Herald
             raise ArgumentError, ":uri not specified for :ping action"
           end
           @uri.scheme = "http" if @uri.scheme.nil? # if missing protocol from URI
+        end
+        
+        # TODO test ping to URL on system and throw exception if fail
+        def test
+          Net::HTTP.new(@uri).head('/').kind_of?(Net::HTTPOK)
+        end
+
+        def notify(title, message)
+          raise "Ping Notifier not implemented yet"
         end
 
       end
