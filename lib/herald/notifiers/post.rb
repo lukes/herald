554eaa0743b5cd703a45b8a6e996c5ea13bb15b7
Luke Duncalfe <l.duncalfe@auckland.ac.nz>
2011-03-22 11:54:29 +1300

    Notifier::Ping and Notifier::Post mirroring each other

--- lib/herald/notifiers/post.rb
+++ lib/herald/notifiers/post.rb
@@ -1,40 +1,47 @@
 class Herald
- class Watcher
-   class Notifier
+  class Watcher
+    class Notifier
 
-     module Post
+      # note most of this code is duplicated between ping and post
+      module Post
 
-       attr_reader :uri
+        attr_reader :uri
 
-       # lazy-load net/http when this Module is used
-       def self.extended(base)
-         Herald.lazy_load('net/http')
-       end
+        # lazy-load net/http when this Module is used
+        def self.extended(base)
+          Herald.lazy_load('net/http')
+        end
 
-       # note dupe between ping and post
-       def parse_options(options)
-         begin
-           @uri = URI.parse(options.delete(:uri) || options.delete(:url))
-           # if URI lib can't resolve a protocol (because it was missing from string)
-           if @uri.class == URI::Generic
-             @uri = URI.parse("http://#{@uri.path}")
-           end
-         rescue URI::InvalidURIError
-           raise ArgumentError, ":uri for :ping action not specified or invalid"
-         end
-       end
+        def parse_options(options)
+          begin
+            @uri = URI.parse(options.delete(:uri) || options.delete(:url))
+            # if URI lib can't resolve a protocol (because it was missing from string)
+            if @uri.class == URI::Generic
+              @uri = URI.parse("http://#{@uri.path}")
+            end
+          rescue URI::InvalidURIError
+            raise ArgumentError, ":uri for :ping action not specified or invalid"
+          end
+        end
 
-       # TODO test ping to URL on system and throw exception if fail
-       def test
-         Net::HTTP.new(@uri.path).head('/').kind_of?(Net::HTTPOK)
-       end
+        # test by pinging to URI throw exception if fail
+        def test
+          response = Net::HTTP.new(@uri.host).head('/')
+          return if response.kind_of?(Net::HTTPOK)
+          # TODO raise custom error types
+          if response.kind_of?(Net::HTTPFound)
+            raise "URI #{@uri} is being redirected to #{response.header['location']}"
+          else
+            raise "URI #{@uri} cannot be reached. Ping returned status code: #{response.code}"
+          end
+        end
 
-       def notify(title, message)
-         Net::HTTP.post_form(@uri.to_s, { "title" => title, "message" => message })
-       end
+        def notify(title, message)
+          Net::HTTP.post_form(@uri.to_s, { "title" => title, "message" => message })
+        end
 
-     end
+      end
 
-   end
- end  
+    end
+  end  
 end
