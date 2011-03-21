5a4ae407a7691824c249fa909b7b90439fc9e695
Luke Duncalfe <l.duncalfe@auckland.ac.nz>
2011-03-21 15:35:41 +1300

    added activities(), which raises an not implemented error currently

--- lib/herald/watchers/rss.rb
+++ lib/herald/watchers/rss.rb
@@ -15,13 +15,18 @@ class Herald
       # spawn thread for each uri given
       def start; puts "Starting..."; end
       def stop; end    
-      def activites; end
 
       def parse_options(options)
         @uri = options.delete(:from).to_a
         if @uri.empty?
           raise ArgumentError, "RSS source not specified in :from Hash"
         end
+      end
+      
+    private
+    
+      def activites
+        raise "Rss Watcher not implemented yet"
       end
 
     end
