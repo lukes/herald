1f7e72db6125802ee912caa433c89cc5db020c46
Luke Duncalfe <l.duncalfe@auckland.ac.nz>
2011-03-21 15:35:02 +1300

    reordered methods

--- lib/herald/notifiers/stdout.rb
+++ lib/herald/notifiers/stdout.rb
@@ -4,6 +4,13 @@ class Herald
 
       module Stdout
 
+        def parse_options(options);
+          # option to send stdout to file
+          if file = options.delete(:file)
+            $stdout = File.new(file, 'w')
+          end
+        end
+        
         # is always working, so true
         def test; true; end
 
@@ -11,13 +18,6 @@ class Herald
         def notify(title, message)
           $stdout.puts "#{title}:\t#{message}"
           $stdout.flush
-        end
-
-        def parse_options(options);
-          # option to send stdout to file
-          if file = options.delete(:file)
-            $stdout = File.new(file, 'w')
-          end
         end
 
       end
