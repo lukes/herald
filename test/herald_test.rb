532968f963aa26022b7f5bc8f115e3a806644e2d
Luke Duncalfe <l.duncalfe@auckland.ac.nz>
2011-03-22 17:34:58 +1300

    Adding tests

--- test/herald_test.rb
+++ test/herald_test.rb
@@ -1,7 +1,13 @@
+require File.dirname(__FILE__) + '/helper'
+
 class HeraldTest < TestCase
   
   test 'Passing a block' do
     assert Herald.watch {}
+  end
+  
+  test 'lazy loading module' do
+    
   end
   
 end
