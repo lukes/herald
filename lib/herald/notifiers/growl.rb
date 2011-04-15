862438dc1fe04515a0a3fadc3794a8cb9c7df07c
Luke <lduncalfe@eml.cc>
2011-04-15 20:00:49 +1200

    Notifier::Growl switched from using ruby-growl cli, to using the classes

--- lib/herald/notifiers/growl.rb
+++ lib/herald/notifiers/growl.rb
@@ -4,27 +4,29 @@ class Herald
 
       module Growl
 
-        # no options to parse for Growl
-        def parse_options(options); end
-        
-        # test growl on system and throw exception if fail.
-        def test
-          # TODO suppress output
-          unless(system("growl --version"))
-            # TODO throw custom exception
-            raise "ruby-growl gem is not installed. Run:\n\tsudo gem install ruby-growl"
-          end
+        @@notification_type = "Herald Notification"
+        attr_accessor :growl, :sticky
+
+        def self.extended(base)
+          Herald.lazy_load('ruby-growl')
         end
+
+        def parse_options(options)
+          @sticky = options.delete(:sticky) || false
+          @priority = options.delete(:priority) || 1
+          host = options.delete(:host) || "localhost"
+          pass = options.delete(:pass) || nil
+          @growl = ::Growl.new(host, "herald", [@@notification_type], [@@notification_type], pass)
+        end
+        
+        # no tests for Growl
+        def test; end
 
         # send a Growl notification
         def notify(item)
-          # response will be false if system() call exits with an error.
-          # presence of ruby-growl has already been tested in test(), so a false
-          # return is likely to be the user's Growl settings
-          # TODO escape characters in title and message
-          item.title.gsub!("'", '')
-          item.message.gsub!("'", '')
-          if !system("growl -H localhost -t '#{item.title}' -m '#{item.message}'")
+          begin
+            @growl.notify(@@notification_type, item.title, item.message, @priority, @sticky)
+          rescue Errno::ECONNREFUSED => e
             # TODO throw custom exception
             raise "Growl settings not configured to allow remote application registration. See Growl website docs: http://growl.info/documentation/exploring-preferences.php"
           end
