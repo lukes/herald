bcbf619f13e3c38a9c3ebe8b6fe43f11ffc35822
Luke Duncalfe <l.duncalfe@auckland.ac.nz>
2011-05-03 11:31:08 +1200

    Notifier::Post much more robust, can take unideal (according to net/http) uris

--- lib/herald/notifiers/post.rb
+++ lib/herald/notifiers/post.rb
@@ -14,14 +14,23 @@ class Herald
         end
 
         def parse_options(options)
+          uri = options.delete(:uri) || options.delete(:url)
+          unless uri
+            raise ArgumentError, ":uri for :ping action not specified"
+          end
           begin
-            @uri = URI.parse(options.delete(:uri) || options.delete(:url))
+            uri = URI.parse(URI.encode(uri))
             # if URI lib can't resolve a protocol (because it was missing from string)
-            if @uri.class == URI::Generic
-              @uri = URI.parse("http://#{@uri.path}")
+            if uri.class == URI::Generic
+              uri = URI.parse("http://#{uri.path}")
             end
+            # add trailing slash if nil path. path() will return nil
+            # if uri passed was a domain missing a trailing slash. net/http's post
+            # methods require the trailing slash to be present otherwise they fail
+            uri.path = "/" if uri.path.empty?
+            @uri = uri
           rescue URI::InvalidURIError
-            raise ArgumentError, ":uri for :ping action not specified or invalid"
+            raise ArgumentError, ":uri for :ping action invalid"
           end
         end
 
@@ -30,15 +39,11 @@ class Herald
           response = Net::HTTP.new(@uri.host).head('/')
           return if response.kind_of?(Net::HTTPOK)
           # TODO raise custom error types
-          if response.kind_of?(Net::HTTPFound)
-            raise "URI #{@uri} is being redirected to #{response.header['location']}"
-          else
-            raise "URI #{@uri} cannot be reached. Ping returned status code: #{response.code}"
-          end
+          raise "#{response.code} status code returned for URI #{@uri}. 200 code expected"
         end
 
         def notify(item)
-          Net::HTTP.post_form(@uri.to_s, { "title" => item.title, "message" => item.message })
+          Net::HTTP.post_form(@uri, { "title" => item.title, "message" => item.message }.merge(item.data))
         end
 
       end
