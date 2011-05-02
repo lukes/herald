ad38fcdf72462dfaf61e83332f3a1898e98e1bc5
Luke Duncalfe <l.duncalfe@auckland.ac.nz>
2011-05-03 11:29:50 +1200

    Generic error handling for Notifier::Ping

--- lib/herald/notifiers/ping.rb
+++ lib/herald/notifiers/ping.rb
@@ -14,14 +14,18 @@ class Herald
 
         # note: dupe between ping and post
         def parse_options(options)
+          uri = options.delete(:uri) || options.delete(:url)
+          unless uri
+            raise ArgumentError, ":uri for :ping action not specified"
+          end
           begin
-            @uri = URI.parse(options.delete(:uri) || options.delete(:url))
+            @uri = URI.parse(uri)
             # if URI lib can't resolve a protocol (because it was missing from string)
             if @uri.class == URI::Generic
               @uri = URI.parse("http://#{@uri.path}")
             end
           rescue URI::InvalidURIError
-            raise ArgumentError, ":uri for :ping action not specified or invalid"
+            raise ArgumentError, ":uri for :ping action invalid"
           end
         end
         
@@ -29,11 +33,7 @@ class Herald
           response = Net::HTTP.new(@uri.host).head('/')
           return if response.kind_of?(Net::HTTPOK)
           # TODO raise custom error types
-          if response.kind_of?(Net::HTTPFound)
-            raise "URI #{@uri} is being redirected to #{response.header['location']}"
-          else
-            raise "URI #{@uri} cannot be reached. Ping returned status code: #{response.code}"
-          end
+          raise "#{response.code} status code returned for URI #{@uri}. 200 code expected"
         end
 
         def notify(item)
