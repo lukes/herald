3b7cbee6fd059492a8414833848bb61ce9551ef1
Luke Duncalfe <l.duncalfe@auckland.ac.nz>
2011-05-03 16:27:00 +1200

    Notifier::Ping now accepts array of uris

--- lib/herald/notifiers/ping.rb
+++ lib/herald/notifiers/ping.rb
@@ -8,36 +8,44 @@ class Herald
         def self.extended(base)
           Herald.lazy_load('net/http')
           class << base
-            attr_accessor :uri
+            attr_accessor :uris
           end
         end
 
         # note: dupe between ping and post
         def parse_options(options)
-          uri = options.delete(:uri) || options.delete(:url)
-          unless uri
+          @uris = []
+          uris = Array(options.delete(:uri) || options.delete(:url) || options.delete(:uris) || options.delete(:urls))
+          if uris.empty?
             raise ArgumentError, ":uri for :ping action not specified"
           end
-          begin
-            @uri = URI.parse(uri)
-            # if URI lib can't resolve a protocol (because it was missing from string)
-            if @uri.class == URI::Generic
-              @uri = URI.parse("http://#{@uri.path}")
+          uris.each do |uri|
+            begin
+              uri = URI.parse(uri)
+              # if URI lib can't resolve a protocol (because it was missing from string)
+              if uri.class == URI::Generic
+                uri = URI.parse("http://#{uri.path}")
+              end
+              @uris << uri
+            rescue URI::InvalidURIError
+              raise ArgumentError, ":uri for :ping action invalid"
             end
-          rescue URI::InvalidURIError
-            raise ArgumentError, ":uri for :ping action invalid"
           end
         end
         
         def test
-          response = Net::HTTP.new(@uri.host).head('/')
-          return if response.kind_of?(Net::HTTPOK)
-          # TODO raise custom error types
-          raise "#{response.code} status code returned for URI #{@uri}. 200 code expected"
+          @uris.each do |uri|
+            response = Net::HTTP.new(uri.host).head('/')
+            return if response.kind_of?(Net::HTTPOK)
+            # TODO raise custom error types
+            raise "#{response.code} status code returned for URI #{uri}. 200 code expected"
+          end
         end
 
         def notify(item)
-          Net::HTTP.new(@uri.host).head('/')
+          @uris.each do |uri|
+            Net::HTTP.new(uri.host).head('/')
+          end
         end
 
       end
