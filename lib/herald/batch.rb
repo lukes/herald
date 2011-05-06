1d98688833c836f50e26b9d3d9774f091dd5173a
Luke Duncalfe <l.duncalfe@auckland.ac.nz>
2011-05-06 15:31:31 +1200

    Moving some batch operations to a separate module

---
+++ lib/herald/batch.rb
@@ -0,0 +1,55 @@
+class Herald
+
+  # batch methods   
+  module Batch
+
+    def start
+      heralds.each do |herald|
+        herald.start
+      end
+      self
+    end
+
+    def stop
+      heralds.each do |herald|
+        herald.stop
+      end
+      self
+    end
+
+    # stop all heralds, and remove them
+    # from list of herald instances. mostly
+    # useful for clearing @@heralds when testing
+    def clear
+      stop()
+      heralds.clear
+      true
+    end
+
+    def alive?
+      heralds.any? { |h| h.alive? }
+    end
+
+    # returns size of @@heralds
+    # can optionally take :alive or :stopped
+    def size(obj = nil)
+      heralds(obj).size
+    end
+
+    # can optionally take a herald object or :stopped
+    def delete(obj = nil)
+      case
+      when obj.is_a?(Herald)
+        Array(obj)
+      else
+        heralds(obj)
+      # with the selected array of heralds
+      end.each do |h| 
+        h.delete # call each herald's delete method
+      end
+      self
+    end
+    
+  end
+  
+end
